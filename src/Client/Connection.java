1d6afdc0a3defcf4152b7d6fa2893362b6bf327a
aliasliao <liaojinyuan@zju.edu.cn>
2017-01-03 00:40:17 +0800

    jar img

--- src/Client/Connection.java
+++ src/Client/Connection.java
@@ -13,7 +13,8 @@ import java.net.Socket;
  */
 public class Connection implements Runnable {
 
-    private final String ip = "123.206.186.94";
+//    private final String ip = "123.206.186.94";
+    private final String ip = "localhost";
     private final int port = 4396;
     private ObjectInputStream input;
     private ObjectOutputStream output;
